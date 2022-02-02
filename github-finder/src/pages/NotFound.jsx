5df02056b9b669edecad743ffbb02021e605c3d5
leannelb <leannelaceybyrne@outlook.com>
2022-02-02 20:49:08 +0000

    test for vercel deploy

--- github-finder/src/pages/NotFound.jsx
+++ github-finder/src/pages/NotFound.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FaHome } from 'react-icons/fa';
 
+// test for vercel deploy
 function NotFound() {
     return (
         <div>
